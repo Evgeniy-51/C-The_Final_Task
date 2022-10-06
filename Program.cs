b35c754170a1ec84145c90105469b17848d86b8c
Evgeniy-51 <evgeniy-51@yandex.ru>
2022-10-06 21:44:07 +0300

    add program

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿string[] array = { "hello", "2", "world", ":-)" };
+// string[] array = { "1234", "1567", "-2", "computer science" };
+// string[] array = { "Russia", "Denmark", "Kazan" };
+int index = 0;
+string[] newArr = new string[0];
+
+void pushFunction(int index, string elem)
+{
+    string[] tempArr = new string[index];
+    tempArr[index - 1] = elem;
+    for (int i = 0; i < newArr.Length; i++)
+    {
+        tempArr[i] = newArr[i];
+    }
+    newArr = tempArr;
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i].Length <= 3)
+    {
+        index++;
+        pushFunction(index, array[i]);
+    }
+}
+
+Console.WriteLine(String.Join(",  ", newArr));
